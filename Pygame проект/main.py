4827594ba7bb39a9c2bd4e16ea5b313578108191
Сонхи <corochkannn@yandex.ru>
2025-03-01 21:13:17 +0300

    Big upd
    Fireballs added, VFX updated, project description updated.

--- Pygame проект/main.py
+++ Pygame проект/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import math
 
 import pygame
 import sqlite3
@@ -11,21 +12,21 @@ pygame.display.set_caption("IMperfect World")
 conn2 = sqlite3.connect('Data/Settings.db')
 cur2 = conn2.cursor()
 size = width, height = 1366, 768
-cur2.execute("SELECT sizes FROM Objects WHERE screen_info = ?", ('1',))
-els = cur2.fetchone()[0].split(':')
-conn2.close()
 screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
-tile_group = pygame.sprite.Group()
 wall_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
+hit_enemy_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+particle_group = pygame.sprite.Group()
 conn1 = sqlite3.connect('Data/data-iw.db')
 cur = conn1.cursor()
 arial_100 = pygame.font.SysFont('arial', 100)
 nav = ''
-quest = 'Сбеги через дверь!'
+score = 100
+m1_cooldown = 5
 active = False
 activeG = False
 game = False
@@ -56,6 +57,8 @@ player_walk_image = load_image('Hero/Rogue/Run/Run-Sheet1.png', -1)
 enemy_image = load_image('Enemy/Skeleton Crew/Skeleton - Base/Idle/Idle-Sheet.png', -1)
 enemy_walk_image = load_image('Enemy/Skeleton Crew/Skeleton - Base/Run/Run-Sheet.png', -1)
 quest_background = load_image('Environment/Dungeon Prison/Assets/Quest_Background.png')
+bullet_img = load_image('Environment/Dungeon Prison/Assets/bullet.png', -1)
+hit_img = load_image('Environment/Dungeon Prison/Assets/hit.png', -1)
 tile_images = {
     '1': 'Environment/Dungeon Prison/Assets/Boxwall.png',
     '2': 'Environment/Dungeon Prison/Assets/barrelwall.pngp',
@@ -163,7 +166,7 @@ class Menu:
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, typ, x, y, tile_width, tile_height):
-        super().__init__(tile_group, all_sprites)
+        super().__init__(all_sprites)
         if typ != '3':
             wall_group.add(self)
         else:
@@ -179,7 +182,6 @@ class Tile(pygame.sprite.Sprite):
 def tile_update(el):
     typ, x, y, w, h = el.split('/')
     Tile(typ, int(x), int(y), int(w), int(h))
-    # 1/523/568/10/9:1/683/500/10/9:2/513/476/10/9:3/795/100/40/40:2/805/250/10/9:4/534/250/1/2:4/557/260/1/2:4/537/265/1/2:4/560/272/1/2
 
 
 class Player(pygame.sprite.Sprite):
@@ -219,6 +221,7 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y):
         global game, enemy_walking
         super().__init__(enemy_group, all_sprites)
+        self.health = 100
         self.frames_standing = []
         self.frames_walking = []
         self.sheet1 = enemy_image
@@ -248,9 +251,85 @@ class Enemy(pygame.sprite.Sprite):
             return self.image
 
 
-def create_enemies():
-    for i in range(5):  # Создаем 5 врагов
-        enemy = Enemy(600 + i * 80, 250)  # Начальная позиция врагов
+def create_enemies(settings):
+    n, x, y, a, b = map(int, settings.split('/'))
+    for i in range(n):
+        enemy = Enemy(x + i * a, y + i * b)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(bullet_group, all_sprites)
+        self.speed_x = 0
+        self.speed_y = 0
+        self.speed = 25
+        self.frames = []
+        self.sheet1 = bullet_img
+        self.cut_sheet(self.sheet1, 3, 1, self.frames)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.image.get_rect().move(1, 1)
+        self.rect.topleft = (x, y)
+
+    def cut_sheet(self, sheet, columns, rows, frames_list):
+        rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (rect.w * i, rect.h * j)
+                frames_list.append(sheet.subsurface(pygame.Rect(frame_location, rect.size)))
+
+    def update(self):
+        if game:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            return self.image
+
+    def find_path(self, dest_x, dest_y):
+        delta_x = dest_x - self.rect.centerx
+        delta_y = dest_y - self.rect.centery
+        distance = math.sqrt(delta_x ** 2 + delta_y ** 2)
+
+        if distance != 0:
+            self.speed_x = (delta_x / distance) * self.speed
+            self.speed_y = (delta_y / distance) * self.speed
+
+    def move_to(self):
+        global screen, bullet_group, wall_group
+        self.rect.left += self.speed_x
+        self.rect.top += self.speed_y
+
+        if self.rect.left > 455 and self.rect.right < 995 \
+                and self.rect.top > 119 and self.rect.bottom < 710:
+            if not pygame.sprite.spritecollideany(self, wall_group):
+                return True
+        return False
+
+
+class Particle(pygame.sprite.Sprite):
+    def __init__(self, x, y, sheet, columns, rows):
+        super().__init__(particle_group, all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows, self.frames)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.image.get_rect().move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows, frames_list):
+        rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (rect.w * i, rect.h * j)
+                frames_list.append(sheet.subsurface(pygame.Rect(frame_location, rect.size)))
+
+    def update(self):
+        if game:
+            if self.cur_frame != 3:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                return self.image
+            else:
+                self.kill()
+
 
 
 class Game:
@@ -258,11 +337,16 @@ class Game:
         self.end_time = None
 
     def open_game(self, args):
-        global game, active, activeG, player
+        global game, active, activeG, els
         game, active, activeG = True, False, False
-        i, folder = args.split(',')
-        self.load_map(folder, i)
-        create_enemies()
+        self.i, self.folder = args.split(',')
+        self.load_map(self.folder, self.i)
+        cur2.execute("SELECT setting FROM Mission WHERE number = ?", (self.i,))
+        els, self.en_settings = cur2.fetchone()[0].split(';')
+        els = els.split(':')
+        # 1/753/508/10/9:1/603/545/10/9:3/795/100/40/40:2/600/200/10/9:4/900/290/1/2:4/919/289/1/2;8/550/210/40/50
+        # 1/523/568/10/9:1/683/500/10/9:2/513/476/10/9:3/795/100/40/40:2/805/250/10/9:4/534/250/1/2:4/557/260/1/2:4/537/265/1/2:4/560/272/1/2;5/600/250/80/50
+        create_enemies(self.en_settings)
 
     def load_map(self, folder, i):
         global gamefon
@@ -270,7 +354,7 @@ class Game:
         gamefon = pygame.transform.scale(load_image(self.path), (1000, 720))
         screen.blit(gamefon, (0, 0))
 
-    def game_end(self, score, i=1):
+    def game_end(self, i=1):
         global game, active, running
         game = False
         active = True
@@ -281,7 +365,7 @@ class Game:
         self.end_time = pygame.time.get_ticks()
         if i == 1:
             self.font = pygame.font.Font('Data/Courier.ttf', 100)
-            self.string_rendered = self.font.render(score, 1, pygame.Color('white'))
+            self.string_rendered = self.font.render(str(score), 1, pygame.Color('white'))
             self.score_rect = self.string_rendered.get_rect()
             self.score_rect.left, self.score_rect.top = 314, 382
 
@@ -364,14 +448,10 @@ player = Player(600, 655)
 menu.append_option('Играть', OpenSMenu)
 menu.append_option('Выход', Quit)
 gamemenu.append_option('Миссия 1', mission.open_game, '1,Dungeon Prison/Assets')
+gamemenu.append_option('Миссия 2', mission.open_game, '2,Dungeon Prison/Assets')
 gamemenu.append_option('Назад', gamemenu.Back)
-# player, level_x, level_y = generate_level(load_level('map.txt'))
 running = play
 enemys_update_timer = 0
-Qfont = pygame.font.SysFont('arial', 42)
-Qstring_rendered = Qfont.render(quest, 1, pygame.Color('black'))
-Quest_rect = Qstring_rendered.get_rect()
-Quest_rect.left, Quest_rect.top = 35, 68
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -400,6 +480,7 @@ while running:
             and (event.key == pygame.K_SPACE)):
                 gamemenu.select()
         if game:
+            m1_cooldown -= 1
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     nav += 'a'
@@ -415,7 +496,14 @@ while running:
                     player_walking = True
                 if event.key == pygame.K_e:
                     if pygame.sprite.spritecollideany(player, exit_group):
-                        mission.game_end('100')
+                        mission.game_end()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if m1_cooldown <= 0:
+                    bullet = Bullet(player.rect.left, player.rect.top)
+                    b_coords = pygame.mouse.get_pos()
+                    bullet.find_path(b_coords[0], b_coords[1])
+                    m1_cooldown = 5
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
@@ -434,12 +522,23 @@ while running:
         gamemenu.draw(100, 100, 100)
     elif game:
         screen.blit(gamefon, (0, 0))
-        screen.blit(quest_background, (20, 50))
-        screen.blit(Qstring_rendered, Quest_rect)
+        for bullet in bullet_group:
+            if not bullet.move_to():
+                bullet.kill()
+            else:
+                for enemy in enemy_group:
+                    hit_enemy_group.add(enemy)
+                    if pygame.sprite.spritecollideany(bullet, hit_enemy_group):
+                        particle = Particle(bullet.rect.left, bullet.rect.top, hit_img, 4, 1)
+                        enemy.health -= 20
+                        bullet.kill()
+                        if enemy.health <= 0:
+                            score += 20
+                            enemy.kill()
+                    hit_enemy_group.remove(enemy)
 
         for el in els:
             tile_update(el)
-        clock.tick(12)
 
         if nav != '':
             for el in nav:
@@ -473,7 +572,7 @@ while running:
         else:
             player_walking = False
 
-        enemy_update_interval = random.randint(1, 4)
+        enemy_update_interval = random.randint(1, 3)
         enemys_update_timer += 1
 
         if enemys_update_timer >= enemy_update_interval:
@@ -486,7 +585,7 @@ while running:
                 else:
                     move = random.choice(['W', 'A', 'S', 'D'])
                     enemy_walking = True
-                    enemy_speed = random.randint(15, 25)
+                    enemy_speed = random.randint(8, 15)
 
                     if move == 'W':
                         enemys.rect.top -= enemy_speed
@@ -513,14 +612,16 @@ while running:
                         elif enemys.rect.right > 995:
                             enemys.rect.right = 995
                 if pygame.sprite.spritecollideany(enemys, player_group):
-                    mission.game_end('000', 2)
+                    mission.game_end(2)
 
         all_sprites.update()
         all_sprites.draw(screen)
         player_group.draw(screen)
         enemy_group.draw(screen)
+        particle_group.draw(screen)
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(10)
 conn1.close()
+conn2.close()
 pygame.quit()
